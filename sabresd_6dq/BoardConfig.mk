4249aea110b3d85cb5f0e43188f6075f9974b5f0
Eric Nelson <eric.nelson@boundarydevices.com>
2015-03-15 15:34:06 -0700

    sabresd_6dq: allow compilation with Boundary U-Boot
    Signed-off-by: Eric Nelson <eric.nelson@boundarydevices.com>

--- sabresd_6dq/BoardConfig.mk
+++ sabresd_6dq/BoardConfig.mk
@@ -120,7 +120,7 @@ IMX_CAMERA_HAL_V2 := true
 #define consumer IR HAL support
 IMX6_CONSUMER_IR_HAL := true
 
-TARGET_BOOTLOADER_CONFIG := imx6q:mx6qsabresdandroid_config imx6dl:mx6dlsabresdandroid_config
+TARGET_BOOTLOADER_CONFIG := imx6q:mx6qsabresd_config imx6dl:mx6dlsabresd_config
 TARGET_BOARD_DTS_CONFIG := imx6q:imx6q-sabresd.dtb imx6dl:imx6dl-sabresd.dtb imx6q-ldo:imx6q-sabresd-ldo.dtb
 
 BOARD_SEPOLICY_DIRS := \
